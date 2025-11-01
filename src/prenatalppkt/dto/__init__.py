a65cd31cbad64764ebcd4124fc3380a94eb24ea9
VarenyaJ <varenyajj@gmail.com>
2025-11-01 16:15:25 +0100

    expand inits

--- src/prenatalppkt/dto/__init__.py
+++ src/prenatalppkt/dto/__init__.py
@@ -1,3 +1,4 @@
 from .exam_data import ExamData
+from .fetus_data import FetusData
 
-__all__ = ["ExamData"]
+__all__ = ["ExamData", "FetusData"]
--- src/prenatalppkt/parser/observer/__init__.py
+++ src/prenatalppkt/parser/observer/__init__.py
@@ -1,3 +1,4 @@
 from .exam_data_parser import ExamDataParser
+from .fetus_parser import FetusParser
 
-__all__ = ["ExamDataParser"]
+__all__ = ["ExamDataParser", "FetusParser"]
