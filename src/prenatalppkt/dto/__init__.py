f25c62249d724b4af60b783714a8f1e696c088d7
VarenyaJ <varenyajj@gmail.com>
2025-11-04 14:23:33 +0100

    Add FetusRatiosParser and FetusRatiosData DTO; integrate into FetusParser

--- src/prenatalppkt/dto/__init__.py
+++ src/prenatalppkt/dto/__init__.py
@@ -1,5 +1,13 @@
 from .exam_data import ExamData
 from .fetus_data import FetusData
 from .measurements_data import MeasurementsData, Measurement
+from .ratios_data import Ratio, FetusRatiosData
 
-__all__ = ["ExamData", "FetusData", "MeasurementsData", "Measurement"]
+__all__ = [
+    "ExamData",
+    "FetusData",
+    "MeasurementsData",
+    "Measurement",
+    "Ratio",
+    "FetusRatiosData",
+]
--- src/prenatalppkt/parser/observer/fetus/__init__.py
+++ src/prenatalppkt/parser/observer/fetus/__init__.py
@@ -1,5 +1,11 @@
 from .fetus_anatomy_text_parser import FetusAnatomyTextParser
 from .fetus_fetus_parser import FetusFetusParser
 from .fetus_measurements_parser import FetusMeasurementsParser
+from .fetus_ratios_parser import FetusRatiosParser
 
-__all__ = ["FetusAnatomyTextParser", "FetusFetusParser", "FetusMeasurementsParser"]
+__all__ = [
+    "FetusAnatomyTextParser",
+    "FetusFetusParser",
+    "FetusMeasurementsParser",
+    "FetusRatiosParser",
+]
