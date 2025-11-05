4a9234697e6f1e3dcaed12b6b317e5ee6020202e
VarenyaJ <varenyajj@gmail.com>
2025-11-05 14:53:54 +0100

    Add FetusEfwParser and FetusEfwData DTO; integrate EFW parsing into FetusParser

--- src/prenatalppkt/dto/__init__.py
+++ src/prenatalppkt/dto/__init__.py
@@ -2,12 +2,15 @@ from .exam_data import ExamData
 from .fetus_data import FetusData
 from .measurements_data import MeasurementsData, Measurement
 from .ratios_data import Ratio, FetusRatiosData
+from .efw_data import EfwEntry, FetusEfwData
+
 
 __all__ = [
-    "ExamData",
-    "FetusData",
-    "MeasurementsData",
-    "Measurement",
-    "Ratio",
-    "FetusRatiosData",
+   "ExamData",
+   "FetusData",
+   "MeasurementsData",
+   "Measurement",
+   "Ratio",
+   "FetusRatiosData",
+   "FetusEfwData",
 ]
---
+++ tests/observer/fetus/test_fetus_efw_parser.py
@@ -0,0 +1,40 @@
+from prenatalppkt.parser.observer.fetus.fetus_efw_parser import FetusEfwParser
+
+sample_efw_json = {
+   "efws": [
+       {
+           "fetus_number": 1,
+           "label": "EFW (AC, FL, HC)",
+           "value": 1014.828,
+           "decimal_paces": 0,
+           "calculated_percentile": 55.6,
+           "percentile_for_display": "56%",
+           "print_in_report": 1,
+           "range": ""
+       },
+       {
+           "fetus_number": 1,
+           "label": "EFW (AC, FL)",
+           "value": 1042.214,
+           "decimal_paces": 0,
+           "calculated_percentile": 63.7,
+           "percentile_for_display": "64%",
+           "print_in_report": 0,
+           "range": ""
+       }
+   ]
+}
+
+
+def test_parse_efw_basic():
+   parser = FetusEfwParser()
+   data = parser.parse(sample_efw_json)
+
+   assert data is not None
+   assert data.fetus_number == 1
+   assert data.efw_count == 2
+
+   efw = data.get_efw_by_label("EFW (AC, FL, HC)")
+   assert efw is not None
+   assert abs(efw.value - 1014.828) < 1e-5
+   assert efw.percentile_for_display == "56%"
