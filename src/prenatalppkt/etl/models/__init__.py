5bd785cc9d04e38bb3c8064cf8e0a29ce8ba23c8
VarenyaJ <varenyajj@gmail.com>
2025-11-26 14:12:02 +0100

    remove confusing structures

--- src/prenatalppkt/etl/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data models for ETL pipeline.
-
-Contains simple data classes for representing extracted biometry measurements.
-"""
-
-from prenatalppkt.etl.models.biometry import Biometry, BiometryCollection
-
-__all__ = ["Biometry", "BiometryCollection"]
