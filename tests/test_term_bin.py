d5bd59ada4d18561fd4286d0a8caa28dead399da
VarenyaJ <varenyajj@gmail.com>
2026-06-08 14:08:24 -0400

    feat(term_bin): add LOINC + raw measurement fields

--- tests/test_term_bin.py
+++ tests/test_term_bin.py
@@ -38,3 +38,57 @@ class TestTermBin:
         prange = PercentileRange.above_97p()
         assert prange.upper is None
         assert prange.lower == Percentile.Ninetyseventh
+
+    def test_loinc_and_value_fields_default_to_none(self):
+        """LOINC + raw-value fields default to None for back-compat."""
+        bin = TermBin(
+            range=PercentileRange.between_10p_50p(),
+            hpo_id="HP:0000240",
+            hpo_label="Abnormality of skull size",
+            normal=True,
+        )
+
+        assert bin.loinc_code is None
+        assert bin.loinc_label is None
+        assert bin.value_mm is None
+        assert bin.gestational_age_weeks is None
+
+    def test_loinc_and_value_fields_round_trip(self):
+        """Optional LOINC + raw-value fields are stored and equality-relevant."""
+        prange = PercentileRange.between_50p_90p()
+        bin_a = TermBin(
+            range=prange,
+            hpo_id="HP:0000240",
+            hpo_label="Abnormality of skull size",
+            normal=True,
+            loinc_code="LOINC:11984-2",
+            loinc_label="Fetal Head Circumference US",
+            value_mm=175.0,
+            gestational_age_weeks=20.3,
+        )
+        bin_b = TermBin(
+            range=prange,
+            hpo_id="HP:0000240",
+            hpo_label="Abnormality of skull size",
+            normal=True,
+            loinc_code="LOINC:11984-2",
+            loinc_label="Fetal Head Circumference US",
+            value_mm=175.0,
+            gestational_age_weeks=20.3,
+        )
+        bin_c = TermBin(
+            range=prange,
+            hpo_id="HP:0000240",
+            hpo_label="Abnormality of skull size",
+            normal=True,
+            loinc_code="LOINC:11984-2",
+            loinc_label="Fetal Head Circumference US",
+            value_mm=180.0,
+            gestational_age_weeks=20.3,
+        )
+
+        assert bin_a.loinc_code == "LOINC:11984-2"
+        assert bin_a.value_mm == 175.0
+        assert bin_a.gestational_age_weeks == 20.3
+        assert bin_a == bin_b
+        assert bin_a != bin_c
