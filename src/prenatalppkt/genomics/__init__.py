6dd1da855b3d8b8a0e0600927d17786b87a28efd
VarenyaJ <varenyajj@gmail.com>
2026-06-18 11:11:08 -0400

    feat(genomics): re-export genomic builders from package init

--- src/prenatalppkt/genomics/__init__.py
+++ src/prenatalppkt/genomics/__init__.py
@@ -1,5 +1,11 @@
 """Genomics: VCF scanning and Phenopacket v2 genomic builders."""
 
+from prenatalppkt.genomics.genomic import (
+    build_genomic_interpretation,
+    build_vcf_file_entry,
+    to_variation_descriptor,
+    to_vcf_record,
+)
 from prenatalppkt.genomics.vcf import (
     VcfVariant,
     scan_vcf_archive,
@@ -7,4 +13,13 @@ from prenatalppkt.genomics.vcf import (
     scan_vcf_text,
 )
 
-__all__ = ["VcfVariant", "scan_vcf_archive", "scan_vcf_file", "scan_vcf_text"]
+__all__ = [
+    "VcfVariant",
+    "build_genomic_interpretation",
+    "build_vcf_file_entry",
+    "scan_vcf_archive",
+    "scan_vcf_file",
+    "scan_vcf_text",
+    "to_variation_descriptor",
+    "to_vcf_record",
+]
