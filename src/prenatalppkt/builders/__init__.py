3550c43ee155848f7324d3bf855c1e2ed8c72137
VarenyaJ <varenyajj@gmail.com>
2026-07-20 10:33:56 -0400

    style: reformat builders/__init__.py __all__ list with ruff

--- src/prenatalppkt/builders/__init__.py
+++ src/prenatalppkt/builders/__init__.py
@@ -4,4 +4,8 @@ from prenatalppkt.builders.gyn_phenopacket import build_gyn_phenopacket
 from prenatalppkt.builders.observer_phenopacket import build_observer_phenopacket
 from prenatalppkt.builders.viewpoint_phenopacket import build_viewpoint_phenopacket
 
-__all__ = ["build_gyn_phenopacket", "build_observer_phenopacket", "build_viewpoint_phenopacket"]
+__all__ = [
+    "build_gyn_phenopacket",
+    "build_observer_phenopacket",
+    "build_viewpoint_phenopacket",
+]
